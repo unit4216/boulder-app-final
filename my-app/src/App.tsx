5d6bc47d72debc4d8b83f212a15b67d3748b01c8
Pablo <pablo.c@westlandreg.com>
2024-04-07 13:07:09 -0400

    working fe

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './AppTailwind.css';
+import React, {useState} from 'react';
+import './App.css';
+import {TextField} from "@mui/material";
 
 function App() {
+    const [textInput, setTextInput] = useState<string>('')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='text-3xl mb-10'>Hello, enter your name:</div>
+          <TextField
+              className='bg-white'
+              variant="outlined"
+              value={textInput}
+              label={'Enter name'}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setTextInput(event.target.value);
+              }}
+          />
+          {textInput && (
+              <div className='mt-10'>Hello, <span className='text-orange-500'>{textInput}</span>!</div>
+          )}
       </header>
     </div>
   );
