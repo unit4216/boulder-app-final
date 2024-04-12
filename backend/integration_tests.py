88a61175b196347cc579e308755cc9168449f3f5
Pablo <pablo.c@westlandreg.com>
2024-04-12 13:23:52 -0400

    int test

--- backend/integration_tests.py
+++ backend/integration_tests.py
@@ -2,14 +2,21 @@ import datetime
 import unittest
 from unittest.mock import Mock
 
-from backend.aqi import get_past_aqi_data, parse_aqi_data, get_predicted_aqi
+from backend.aqi import get_past_aqi_data, parse_aqi_data, get_predicted_aqi, get_next_hour
 
 
 class TestAqiIntegration(unittest.TestCase):
 
     def test_get_past_aqi_data(self):
-
-        results = get_past_aqi_data('42.3601', '71.0589')
+        aqi_data = get_past_aqi_data('42.3601', '71.0589')
+        parsed_data = parse_aqi_data(aqi_data)
+        now = datetime.datetime.now()
+        next_hour = get_next_hour(now)
+        predicted_aqi = get_predicted_aqi(parsed_data, next_hour)
+        print(predicted_aqi)
+        # we can't test the exact AQI since the API call gets the last 7 days from the current day,
+        # but we can verify that the predicted AQI is in a valid range
+        assert 0 < float(predicted_aqi) < 500
 
     # tests integration between `parse_aqi_data` and `get_predicted_aqi`
     def test_get_predicted_aqi_mock(self):
