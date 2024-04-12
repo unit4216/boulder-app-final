2b563537966cc92e3a731d6c38f3a13119b5d6a2
Pablo <pablo.c@westlandreg.com>
2024-04-12 12:06:46 -0400

    init calc

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,51 @@
 from flask import Flask
 import sqlite3
 from flask_cors import CORS
+import requests
+import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/data')
+def get_api_data():
+    url = 'https://air-quality-api.open-meteo.com/v1/air-quality'
+    params = {
+        "latitude": 42.3662,
+        "longitude": 71.0621,
+        "hourly": ["us_aqi"],
+        "past_days": 7,
+        "forecast_days": 0
+    }
+    resp = requests.get(url, params=params)
+    data = resp.json()
+
+    parsed_data = []
+    for index, hour in enumerate(data['hourly']['time']):
+        aqi = data['hourly']['us_aqi'][index]
+        parsed_data.append({"time": hour, "aqi": aqi})
+
+    now = datetime.datetime.now()
+    # this gets hour in 24h format (e.g. 16)
+    current_hour = now.hour
+    # filter for current hour (date is in format ISO but missing seconds e.g. `YYYY-MM-DDTHH:MM`
+    filtered_data = [x for x in parsed_data if int(x["time"].split('T')[1].split(':')[0]) == current_hour]
+
+    aqi_points = [x['aqi'] for x in filtered_data]
+
+    sum = 0
+    for aqi in aqi_points:
+        sum = sum + aqi
+    average = sum / len(aqi_points)
+
+    return str(average)
+
+
 @app.route('/')
 def get_data():
     # create db
-    con = sqlite3.connect("temporary.db")
+    con = sqlite3.connect("database.db")
     cur = con.cursor()
 
     cur.execute('CREATE TABLE IF NOT EXISTS weather(day TEXT, temperature TEXT)')
