084bbf8179fb5601693e65feb83cba11aaa7d672
Pablo <pablo.c@westlandreg.com>
2024-04-12 12:40:20 -0400

    cleaned

---
+++ backend/helpers.py
@@ -0,0 +1,40 @@
+import requests
+import datetime
+
+
+def get_past_aqi_data(latitude: str, longitude: str):
+    url = 'https://air-quality-api.open-meteo.com/v1/air-quality'
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": ["us_aqi"],
+        "past_days": 7,
+        "forecast_days": 0
+    }
+    resp = requests.get(url, params=params)
+    data = resp.json()
+
+    parsed_data = []
+    for index, hour in enumerate(data['hourly']['time']):
+        aqi = data['hourly']['us_aqi'][index]
+        parsed_data.append({"time": hour, "aqi": aqi})
+
+    return parsed_data
+
+
+def get_predicted_aqi(parsed_data: list[dict]):
+    now = datetime.datetime.now()
+    # this gets hour in 24h format (e.g. 16)
+    # we want to get the next hour
+    next_hour = now.hour + 1 if now.hour != 23 else 0
+    # filter for current hour (date is in format ISO but missing seconds e.g. `YYYY-MM-DDTHH:MM`
+    filtered_data = [x for x in parsed_data if int(x["time"].split('T')[1].split(':')[0]) == next_hour]
+
+    aqi_points = [x['aqi'] for x in filtered_data]
+
+    total = 0
+    for aqi in aqi_points:
+        total = total + aqi
+    average = total / len(aqi_points)
+
+    return str(average)
--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import requests
 import datetime
 from flask import request
+from helpers import get_past_aqi_data, get_predicted_aqi
 
 app = Flask(__name__)
 CORS(app)
@@ -12,37 +13,10 @@ CORS(app)
 # gets the AQI number for the last week at the same hour that the user is querying
 @app.route('/aqi')
 def get_aqi():
-    url = 'https://air-quality-api.open-meteo.com/v1/air-quality'
-    params = {
-        "latitude": request.args.get('latitude'),
-        "longitude": request.args.get('longitude'),
-        "hourly": ["us_aqi"],
-        "past_days": 7,
-        "forecast_days": 0
-    }
-    resp = requests.get(url, params=params)
-    data = resp.json()
-
-    parsed_data = []
-    for index, hour in enumerate(data['hourly']['time']):
-        aqi = data['hourly']['us_aqi'][index]
-        parsed_data.append({"time": hour, "aqi": aqi})
-
-    now = datetime.datetime.now()
-    # this gets hour in 24h format (e.g. 16)
-    # we want to get the next hour
-    next_hour = now.hour + 1 if now.hour != 23 else 0
-    # filter for current hour (date is in format ISO but missing seconds e.g. `YYYY-MM-DDTHH:MM`
-    filtered_data = [x for x in parsed_data if int(x["time"].split('T')[1].split(':')[0]) == next_hour]
-
-    aqi_points = [x['aqi'] for x in filtered_data]
-
-    total = 0
-    for aqi in aqi_points:
-        total = total + aqi
-    average = total / len(aqi_points)
-
-    return str(average)
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    parsed_data = get_past_aqi_data(latitude, longitude)
+    return get_predicted_aqi(parsed_data)
 
 
 @app.route('/')
