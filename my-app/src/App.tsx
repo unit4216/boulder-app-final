1fd2ef26fef022cd3bcb7373498568c598f061a5
Pablo <pablo.c@westlandreg.com>
2024-04-12 12:11:02 -0400

    thing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {TextField} from "@mui/material";
 import {getBackend} from "./endpoints";
 
 function App() {
@@ -16,20 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='text-3xl mb-10'>Hello, enter your name:</div>
-          <TextField
-              className='bg-white'
-              variant="outlined"
-              value={textInput}
-              label={'Enter name'}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setTextInput(event.target.value);
-              }}
-          />
-          {textInput && (
-              <div className='mt-10'>Hello, <span className='text-orange-500'>{textInput}</span>!</div>
-          )}
-          <div>Data from database:</div>
+          <div>The air quality index at TD Garden Stadium next hour is predicted to be:</div>
           <div>{data}</div>
       </header>
     </div>
--- my-app/src/endpoints.ts
+++ my-app/src/endpoints.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const getBackend = async () => {
-    const url = 'http://localhost:5000'
+    const url = 'http://localhost:5000/data'
     const res = await axios.get(url)
     return res.data
 }
