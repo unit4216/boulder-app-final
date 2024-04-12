dec8b4c68f0a4e587db6d9d111269422c2a4e828
Pablo <pablo.c@westlandreg.com>
2024-04-12 11:24:18 -0400

    main route

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def get_data():
+    # create db
+    con = sqlite3.connect("temporary.db")
+    cur = con.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS weather(day TEXT, temperature TEXT)')
+    cur.execute(f'INSERT INTO waether VALUES ("TEST day", "92")')
+    con.commit()
+
+    # read the data we just wrote
+    res = cur.execute('SELECT * FROM weather')
+    return res.fetchall()
 
 
 if __name__ == '__main__':
