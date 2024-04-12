9b61db39b17fe273f302ff61cc8af5134f0e24b8
Pablo <pablo.c@westlandreg.com>
2024-04-12 14:07:00 -0400

    get past predictions

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {getAirQualityIndex} from "./endpoints";
-import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {getAirQualityIndex, getPastPredictions} from "./endpoints";
+import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
 
 interface City {
     city: string,
@@ -31,13 +32,56 @@ const airQualityIndexBounds: AqiBound[] = [
     {range: [301, 500], label: 'Hazardous'}
 ]
 
+interface PredictionRow {
+    aqi: string,
+    generated_at: string,
+    latitude: string,
+    longitude: string,
+    prediction_hour: string
+}
+
+const columns: GridColDef[] = [
+    {
+        field: 'aqi',
+        headerName: 'AQI',
+        flex: 1
+    },
+    {
+        field: 'generated_at',
+        headerName: 'Generated At',
+        flex: 1
+    },
+    {
+        field: 'latitude',
+        headerName: 'Latitude',
+        flex: 1
+    },
+    {
+        field: 'longitude',
+        headerName: 'Longitude',
+        flex: 1
+    },
+    {
+        field: 'prediction_hour',
+        headerName: 'Prediction Hour',
+        flex: 1
+    }
+]
+
 function App() {
     const [airQualityIndex, setAirQualityIndex] = useState<string>('')
     const [selectedCity, setSelectedCity] = useState<City>(cityCoordinates[0])
+    const [pastPredictions, setPastPredictions] = useState<PredictionRow[]>([])
 
     useEffect(()=> {
         getAirQualityIndex(selectedCity.latitude, selectedCity.longitude)
             .then(res=>setAirQualityIndex(res))
+            .catch(err=>console.error(err))
+    },[selectedCity])
+
+    useEffect(()=>{
+        getPastPredictions()
+            .then(res=>setPastPredictions(res))
             .catch(err=>console.error(err))
     },[selectedCity])
 
@@ -48,6 +92,8 @@ function App() {
             return numericalQualityIndex >= lowerBound && numericalQualityIndex <= upperBound
         })
 
+    const formattedAqi = parseFloat(airQualityIndex).toFixed(2)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -56,16 +102,33 @@ function App() {
               <Select
                   value={selectedCity}
                   label="Select City"
-                  onChange={(e) => setSelectedCity(e.target.value)}
+                  onChange={(e) => setSelectedCity(e.target.value as City)}
               >
                   {cityCoordinates.map(city => {
+                      // @ts-ignore
                       return <MenuItem value={city}>{city.city}</MenuItem>
                   })}
               </Select>
           </FormControl>
-          <div>The air quality index in {selectedCity.city} next hour is predicted to be:</div>
-          <div>{airQualityIndex}</div>
-          <div>This is: {relevantBound?.label}</div>
+          <div>
+              The air quality index in &nbsp;
+              <span className='font-bold'>{selectedCity.city}</span>
+              &nbsp; next hour is predicted to be &nbsp;
+              <span className='font-bold'>{formattedAqi}</span>
+          </div>
+          <div>
+              This is considered &nbsp;
+              <span className='font-bold'>{relevantBound?.label}</span>
+              &nbsp; on the US Air Quality Index
+          </div>
+          <div className='mt-10 mb-2'>Previous predictions:</div>
+          <Box sx={{ height: 400, width: '50%' }}>
+              <DataGrid
+                  rows={pastPredictions}
+                  columns={columns}
+                  getRowId={() => Math.random()}
+              />
+          </Box>
       </header>
     </div>
   );
--- my-app/src/endpoints.ts
+++ my-app/src/endpoints.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+const baseUrl = 'http://localhost:5000'
 
 export const getAirQualityIndex = async (latitude: number, longitude: number) => {
-    const url = `http://localhost:5000/aqi?latitude=${latitude}&longitude=${longitude}`
+    const url = `${baseUrl}/aqi?latitude=${latitude}&longitude=${longitude}`
+    const res = await axios.get(url)
+    return res.data
+}
+
+export const getPastPredictions = async () => {
+    const url = `${baseUrl}/get-past-predictions`
     const res = await axios.get(url)
     return res.data
 }
